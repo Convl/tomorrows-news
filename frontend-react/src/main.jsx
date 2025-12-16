1a2ca2913481c03205be02584ce20b6dcd9d1229
Constantin van Lijnden <133672374+Convl@users.noreply.github.com>
2025-12-16 19:05:47 +0100

    try to figure out why vercel posts to http instead of https

--- frontend-react/src/main.jsx
+++ frontend-react/src/main.jsx
@@ -1,11 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
-import { ThemeProvider, CssBaseline } from "@mui/material";
-import theme from "./theme";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import App from "./App.jsx";
+import { ThemeModeProvider } from "./contexts/ThemeModeContext.jsx";
 
 // Debug Environment Variables
 console.log("--- DEBUG ENV VARS ---");
@@ -16,22 +14,23 @@ console.log("----------------------");
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      refetchOnWindowFocus: false,
-      retry: 1,
+      staleTime: 30 * 1000,
+      gcTime: 5 * 60 * 1000,
+      refetchOnMount: false,
+      refetchOnWindowFocus: true,
+      refetchOnReconnect: true,
+      retry: 2,
     },
   },
 });
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ThemeProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
+      <ReactQueryDevtools />
+      <ThemeModeProvider>
+        <App />
+      </ThemeModeProvider>
     </QueryClientProvider>
-  </React.StrictMode>
+  </StrictMode>
 );
